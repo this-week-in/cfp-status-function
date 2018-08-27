8ef23fca9038af1fd8f27c101c300ce6c31b0dd9
Josh Long <josh@joshlong.com>
2018-08-26 20:44:27 -0700

    cloudformation-deploy

--- cloudformation-deploy.sh
+++ cloudformation-deploy.sh
@@ -1,26 +1,54 @@
 #!/usr/bin/env bash
 
-should_build=${1:-echo "we assume you\'ve already built the artifact to be deployed."}
-eval $should_build
-zip myfile.zip deployment.zip
 
-jar=`pwd`/target/cfp-status-function-0.0.1-SNAPSHOT-aws.jar
-ls -la $jar
-zip_name=deployment.zip
-zip=`pwd`/$zip_name
-
-zip  $zip $jar
+region=${AWS_REGION:-us-east-1}
 
 
-aws s3 cp $zip s3://cfp-status-function/$zip_name
+function real_deploy (){
 
-stack_name=greetings-stack
+    stack_name=greetings-stack
+    zip_name=deployment.zip
+    region=${AWS_REGION:-us-east-1}
 
-aws cloudformation delete-stack --stack-name $stack_name --region us-east-1
+    jar=${this_dir}/target/cfp-status-function-0.0.1-SNAPSHOT-aws.jar
+    zip=${this_dir}/$zip_name
 
-sleep 10
+    zip ${zip} ${jar}
 
-aws cloudformation create-stack --stack-name $stack_name  --template-body file://./cloudformation.template  --capabilities CAPABILITY_IAM --region us-east-1
-aws cloudformation list-stacks
-echo "S3: $s3_put "
-echo "Cloud Formation:  $result "
+    aws s3 cp ${zip} s3://cfp-status-function/${zip_name}
+    aws cloudformation delete-stack --stack-name $stack_name --region ${region}
+
+    sleep 10
+
+    aws cloudformation create-stack --stack-name $stack_name  --template-body file://./cloudformation.template  --capabilities CAPABILITY_IAM --region ${region}
+    aws cloudformation list-stacks
+
+}
+
+
+function deploy (){
+    stack_name=greetings-stack
+    cmd_to_run=""
+    complete=$( aws cloudformation list-stacks | grep CREATE_COMPLETE )
+    create_complete=CREATE_COMPLETE
+    list_stacks=$(aws cloudformation list-stacks  |  jq '.[] | .[]  | select(.StackStatus=="CREATE_COMPLETE")  | .StackStatus')
+    if [ "1${list_stacks}" == '1"CREATE_COMPLETE"' ]; then
+     cmd_to_run="update"
+    else
+     cmd_to_run="create"
+    fi
+    cmd=${cmd_to_run}-stack
+    echo "aws cloudformation $cmd --stack-name $stack_name .. "
+    echo "Region: $region"
+    echo "Stack Name: $stack_name"
+    aws cloudformation $cmd --stack-name $stack_name  --template-body file://./cloudformation.template --capabilities CAPABILITY_IAM --region ${region}
+}
+
+cd `dirname $0`
+this_dir=`pwd`
+
+if [ "$1x" != "x" ]; then
+    eval $1
+fi
+
+deploy
